8d42221a53d292098226cb6e4c54e80198ee2d30
Boaz Segev <Boaz@2be.co.il>
2016-02-09 22:38:50 -0500

    docstyle comments

--- lib/http-protocol.h
+++ lib/http-protocol.h
@@ -13,20 +13,22 @@ Feel free to copy, use and enjoy according to the license provided.
 #ifndef HTTP_HEAD_MAX_SIZE
 #define HTTP_HEAD_MAX_SIZE 8192  // 8*1024
 #endif
-//////////////////////////////
-// the following structures are defined herein:
+/**
+ the following structures are defined herein:
+*/
 
-// a Procotol suited for Http/1.x servers. The struct must be obtained using a
-// contructor. i.e.:
-//
-//        struct HttpProtocol http = HttpProtocol();
-//
-// the `struct HttpProtocol` objects live on the stack and their memory is
-// automatically released at the end of the block (function/if block.etc').
-//
+/**
+A Procotol suited for Http/1.x servers. The struct must be obtained using a
+contructor. i.e.:
+
+       struct HttpProtocol http = HttpProtocol();
+
+the `struct HttpProtocol` objects live on the stack and their memory is
+automatically released at the end of the block (function/if block.etc').
+*/
 struct HttpProtocol;
 
-/// returns a stack allocated, core-initialized, Http Protocol object.
+/** returns a stack allocated, core-initialized, Http Protocol object. */
 struct HttpProtocol HttpProtocol(void);
 
 /************************************************/ /**
@@ -42,20 +44,23 @@ contains a reference to the socket's file descriptor waiting for the response.
 A single connection cannot run two The `on_request` callbacks asynchronously.
  */
 
-// This holds the Http protocol, it's settings and callbacks, such as maximum
-// body size, the on request callback, etc'.
+/** This holds the Http protocol, it's settings and callbacks, such as maximum
+body size, the on request callback, etc'. */
 struct HttpProtocol {
-  /// this is the "parent" protocol class, used internally. do not edit data on
-  /// this class.
-  ///
-  /// This must be the first declaration to allow pointer casting inheritance.
+  /**
+  this is the "parent" protocol class, used internally. do not edit data on this
+  class.
+
+  This must be the first declaration to allow pointer casting inheritance. */
   struct Protocol parent;
-  /// sets the maximum size for a body, in Mb (Mega-Bytes).
+  /** sets the maximum size for a body, in Mb (Mega-Bytes). */
   int maximum_body_size;
-  /// the callback to be performed when requests come in.
+  /** the callback to be performed when requests come in. */
   void (*on_request)(struct HttpRequest* request);
-  /// a public folder for file transfers - allows to circumvent any application
-  /// layer server and simply serve files.
+  /**
+  A public folder for file transfers - allows to circumvent any application
+  layer server and simply serve files.
+  */
   char* public_folder;
 };
 
