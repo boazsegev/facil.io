243f745dac8e92adfd386bcd7a335aa379bda0c5
Boaz Segev <Boaz@2be.co.il>
2016-03-23 18:15:47 -0400

    fix group task pooling

--- src/lib-server.c
+++ src/lib-server.c
@@ -1293,7 +1293,7 @@ static void perform_fd_task(struct FDTask* task) {
 struct GroupTask* new_group_task(server_pt srv) {
   struct GroupTask* ret = NULL;
   pthread_mutex_lock(&srv->task_lock);
-  if (srv->fd_task_pool) {
+  if (srv->group_task_pool) {
     ret = srv->group_task_pool;
     srv->group_task_pool = srv->group_task_pool->next;
     --srv->group_task_pool_size;
