f0e2da99b1e9d0d5111195865d22c6b0439f9d32
Boaz Segev <Boaz@2be.co.il>
2016-08-16 08:29:46 +0000

    add includes for bsd (hopeful)

--- src/http/http_response.c
+++ src/http/http_response.c
@@ -5,6 +5,8 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 /* *****************************************************************************
 Helpers
