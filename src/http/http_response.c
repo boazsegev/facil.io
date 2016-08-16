5bfbb85128fbafc7486e5a7f8972962cf5ce2c0f
Boaz Segev <Boaz@2be.co.il>
2016-08-16 09:36:03 +0000

    more

--- src/http/http_response.c
+++ src/http/http_response.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <time.h>
+#include <sys/resource.h>
 
 /* *****************************************************************************
 Helpers
