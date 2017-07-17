a118223c8b607117d3e1487343a8c03e4b07de50
Bo <Boaz@2be.co.il>
2017-07-17 00:02:00 -0400

    State which files isn the example do what

--- examples/boiler_plates/http/dev/main.c
+++ examples/boiler_plates/http/dev/main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 int main(int argc, char const *argv[]) {
-  /* accept command line arguments and setup default values. */
+  /* accept command line arguments and setup default values, see "cli.c" */
   initialize_cli(argc, argv);
 
-  /* initialize HTTP service */
+  /* initialize HTTP service, see "http_service.h" */
   initialize_http_service();
 
   /* start facil */
   facil_run(.threads = fio_cli_get_int("t"), .processes = fio_cli_get_int("w"));
 
+  /* cleanup CLI, see "cli.c" */
   free_cli();
   return 0;
 }
