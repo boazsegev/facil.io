41ab8a70d063ae9a965d7f147632bc1aa62fd5fc
Bo <Boaz@2be.co.il>
2017-07-22 05:44:06 -0400

    Experiment with a cleaner HTTP/1.x parser

---
+++ dev/maybe/http1_parser.c
@@ -0,0 +1,198 @@
+#ifndef __GNU_SOURCE
+#define __GNU_SOURCE
+#endif
+
+#include "http1_parser.h"
+#include <string.h>
+
+// inline static uint8_t seek2ch(uint8_t **pos, uint8_t *limit, uint8_t ch) {
+//   /* This is library based alternative that is sometimes slower  */
+//   if (*pos >= limit || **pos == ch) {
+//     return 0;
+//   }
+//   uint8_t *tmp = memchr(*pos, ch, limit - (*pos));
+//   if (tmp) {
+//     *pos = tmp;
+//     *tmp = 0;
+//     return 1;
+//   }
+//   *pos = limit;
+//   return 0;
+// }
+
+/* a helper that seeks any char, converts it to NUL and returns 1 if found.
+ * requires at lease 1 character.
+ */
+static inline uint8_t seek2ch(uint8_t **buffer, uint8_t *limit,
+                              const uint8_t c) {
+  /* this single char lookup is better when target is closer... */
+  if (**buffer == c)
+    return 1;
+
+  uint64_t wanted = 0x0101010101010101ULL * c;
+  uint64_t *lpos = (uint64_t *)*buffer;
+  uint64_t *llimit = ((uint64_t *)((uintptr_t)limit & 7));
+
+  for (; lpos < llimit; lpos++) {
+    const uint64_t eq = ~((*lpos) ^ wanted);
+    const uint64_t t0 = (eq & 0x7f7f7f7f7f7f7f7fllu) + 0x0101010101010101llu;
+    const uint64_t t1 = (eq & 0x8080808080808080llu);
+    if ((t0 & t1)) {
+      break;
+    }
+  }
+
+  *buffer = (uint8_t *)lpos;
+  while (*buffer < limit) {
+    if (**buffer == c) {
+      **buffer = 0;
+      return 1;
+    }
+    (*buffer)++;
+  }
+  return 0;
+}
+
+/* a helper that seeks the EOL, converts it to NUL and returns it's length */
+inline static uint8_t seek2eol(uint8_t **pos, uint8_t *limit) {
+  /* single char lookup using library is best when target is far... */
+  if (*pos >= limit)
+    return 0;
+  uint8_t *tmp = memchr(*pos, '\n', limit - (*pos));
+  if (tmp) {
+    *pos = tmp;
+    *tmp = 0;
+    if (tmp[-1] == '\r') {
+      (tmp)[-1] = 0;
+      return 2;
+    }
+    return 1;
+  }
+  *pos = limit;
+  return 0;
+}
+
+size_t http1_fio_parser_fn(struct http1_fio_parser_args_s *args) {
+  uint8_t *start = args->buffer;
+  uint8_t *end = start;
+  uint8_t *stop = start + args->length;
+  uint8_t eol_len = 0;
+#define CONSUMED ((size_t)((uintptr_t)start - (uintptr_t)args->buffer))
+
+  switch (args->parser->state.reserved & 0x0F) {
+  case 0:
+    /* request / response line */
+    /* clear out any leadinng white space */
+    while (*start == '\r' || *start == '\r' || *start == ' ' || *start == 0) {
+      start++;
+    }
+    end = start;
+    if (!(eol_len = seek2eol(&end, stop)))
+      return CONSUMED;
+    if (start[0] >= '1' && start[0] <= '9') {
+      /* HTTP response */
+      args->parser->state.reserved |= 128;
+      uint8_t *tmp = start;
+      if (!seek2ch(&tmp, end, ' '))
+        goto error;
+      if (args->on_http_version(args->parser, (char *)start, tmp - start))
+        goto error;
+      tmp = start = tmp + 1;
+      if (!seek2ch(&tmp, end, ' '))
+        goto error;
+      if (args->on_status(args->parser, atol((char *)start), (char *)(tmp + 1),
+                          end - tmp - eol_len))
+        goto error;
+    } else {
+      /* HTTP request */
+      uint8_t *tmp = start;
+      if (!seek2ch(&tmp, end, ' '))
+        goto error;
+      if (args->on_method(args->parser, (char *)start, tmp - start))
+        goto error;
+      tmp = start = tmp + 1;
+      if (seek2ch(&tmp, end, '?')) {
+        if (args->on_path(args->parser, (char *)start, tmp - start))
+          goto error;
+        tmp = start = tmp + 1;
+        if (!seek2ch(&tmp, end, ' '))
+          goto error;
+        if (tmp - start > 0 &&
+            args->on_query(args->parser, (char *)start, tmp - start))
+          goto error;
+      } else {
+        tmp = start;
+        if (!seek2ch(&tmp, end, ' '))
+          goto error;
+        if (args->on_path(args->parser, (char *)start, tmp - start))
+          goto error;
+      }
+      start = tmp + 1;
+      if (start + 7 >= end)
+        goto error;
+      if (args->on_http_version(args->parser, (char *)start,
+                                end - start - eol_len + 1))
+        goto error;
+    }
+    end = start = end + 1;
+    args->parser->state.reserved |= 1;
+  /* fallthrough */
+  case 1:
+    do {
+      uint8_t t2 = 0;
+      if (!(eol_len = seek2eol(&end, stop)))
+        return CONSUMED;
+      uint8_t *tmp = start;
+      /* test for header ending */
+      if (*start == 0) {
+        end = start = end + 1;
+        args->parser->state.reserved |= 2;
+        goto finished_headers; /* break the do..while loop, not the switch
+                                  statement */
+      }
+      if (!seek2ch(&tmp, end, ':'))
+        goto error;
+      if (*tmp == ' ') {
+        tmp++;
+        t2 = 1;
+      };
+      if (tmp - start - t2 == 14 &&
+          !strncasecmp((char *)start, "Content-Length", 14)) {
+        /* handle the special `content-length` header */
+        args->parser->state.content_length = atol((char *)tmp);
+      }
+      /* perform callback */
+      if (args->on_header(args->parser, (char *)start, tmp - start - t2,
+                          (char *)tmp + 1, end - (tmp + 1) - eol_len))
+        goto error;
+      end = start = end + 1;
+    } while ((args->parser->state.reserved & 2) == 0);
+  finished_headers:
+    if (args->parser->state.content_length == 0)
+      goto finish;
+  /* fallthrough */
+  /*  2 | 1 == 3 */
+  case 3:
+    end = start + args->parser->state.content_length - args->parser->state.read;
+    if (end > stop)
+      end = stop;
+    if (args->on_body_chunk(args->parser, (char *)start, end - start))
+      goto error;
+    args->parser->state.read += end - start;
+    if (args->parser->state.content_length == args->parser->state.read)
+      goto finish;
+    return CONSUMED;
+    break;
+  }
+error:
+  args->on_error(args->parser);
+  return CONSUMED;
+finish:
+  *args->parser = (http1_parser_s){args->parser->udata, {0, 0, 0}};
+  if (((args->parser->state.reserved & 128) ? args->on_response
+                                            : args->on_request)(args->parser))
+    goto error;
+  return CONSUMED;
+}
+
+#undef CONSUMED
---
+++ dev/maybe/http1_parser.h
@@ -0,0 +1,88 @@
+#ifndef H_HTTP1_PARSER_H
+/*
+A callback based parser.
+
+This is an attempt to replace the existing HTTP/1.x parser with something easier
+to maintain and that could be used for an HTTP/1.x client as well.
+*/
+#define H_HTTP1_PARSER_H
+#include <stdint.h>
+#include <stdlib.h>
+
+/** this struct contains the state of the parser. */
+typedef struct http1_parser_s {
+  void *udata;
+  struct {
+    size_t content_length;
+    size_t read;
+    uint8_t reserved;
+  } state;
+} http1_parser_s;
+
+/**
+ * Available options for the parsing function.
+ *
+ * Callbacks should return 0 unless an error occured.
+ */
+struct http1_fio_parser_args_s {
+  /** REQUIRED: the parser object that manages the parser's state. */
+  http1_parser_s *parser;
+  /** REQUIRED: the data to be parsed. */
+  void *buffer;
+  /** REQUIRED: the length of the data to be parsed. */
+  size_t length;
+  /** called when a request was received. */
+  int (*const on_request)(http1_parser_s *parser);
+  /** called when a response was received. */
+  int (*const on_response)(http1_parser_s *parser);
+  /** called when a request method is parsed. */
+  int (*const on_method)(http1_parser_s *parser, char *method,
+                         size_t method_len);
+  /** called when a response status is parsed. the status_str is the string
+   * without the prefixed numerical status indicator.*/
+  int (*const on_status)(http1_parser_s *parser, size_t status,
+                         char *status_str, size_t len);
+  /** called when a request path (excluding query) is parsed. */
+  int (*const on_path)(http1_parser_s *parser, char *path, size_t path_len);
+  /** called when a request path (excluding query) is parsed. */
+  int (*const on_query)(http1_parser_s *parser, char *query, size_t query_len);
+  /** called when a the HTTP/1.x version is parsed. */
+  int (*const on_http_version)(http1_parser_s *parser, char *version,
+                               size_t len);
+  /** called when a header is parsed. */
+  int (*const on_header)(http1_parser_s *parser, char *name, size_t name_len,
+                         char *data, size_t data_len);
+  /** called when a body chunk is parsed. */
+  int (*const on_body_chunk)(http1_parser_s *parser, char *data,
+                             size_t data_len);
+  /** called when a protocol error occured. */
+  int (*const on_error)(http1_parser_s *parser);
+};
+
+/**
+ * Returns the amount of data actually consumed by the parser.
+ *
+ * The value 0 indicates there wasn't enough data to be parsed and the same
+ * buffer (with more data) should be resubmitted.
+ *
+ * A value smaller than the buffer size indicates that EITHER a request /
+ * response was detected OR that the leftover could not be consumed because more
+ * data was required.
+ *
+ * Simply resubmit the reminder of the data to continue parsing.
+ *
+ * A request / response callback automatically stops the parsing process,
+ * allowing the user to adjust or refresh the state of the data.
+ */
+size_t http1_fio_parser_fn(struct http1_fio_parser_args_s *args);
+
+static inline __attribute__((unused)) size_t
+http1_fio_parser(struct http1_fio_parser_args_s args) {
+  return http1_fio_parser_fn(&args);
+}
+#if __STDC_VERSION__ >= 199901L
+#define http1_fio_parser(...)                                                  \
+  http1_fio_parser((struct http1_fio_parser_args_s){__VA_ARGS__})
+#endif
+
+#endif
