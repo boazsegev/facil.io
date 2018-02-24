66dcb2d286e75adf3b6695a075ff6492b8ebb004
Bo <Boaz@2be.co.il>
2018-02-24 10:58:30 -0500

    Repeat testing loop to allow for `malloc` pool performance.
    Both the system’s and the facil.io memory allocators use memory pooling
    techniques. Repeating the tests before increasing the load allows the
    test to better expose the pool’s effects on longer running tasks
    (closer to the network server behavior we are testing for).

--- tests/fio_mem_speed.c
+++ tests/fio_mem_speed.c
@@ -7,111 +7,119 @@
 
 #define TEST_CYCLES_START 128
 #define TEST_CYCLES_END 256
+#define TEST_CYCLES_REPEAT 4
 #define REPEAT_LIB_TEST 0
 
 static size_t test_mem_functions(void *(*malloc_func)(size_t),
                                  void *(*calloc_func)(size_t, size_t),
                                  void *(*realloc_func)(void *, size_t),
                                  void (*free_func)(void *)) {
-  size_t clock_alloc = 0, clock_realloc = 0, clock_free = 0, clock_calloc = 0,
-         fio_optimized = 0, fio_optimized2 = 0, errors = 0;
+  size_t clock_alloc = 0, clock_realloc = 0, clock_free = 0, clock_free2 = 0,
+         clock_calloc = 0, fio_optimized = 0, fio_optimized2 = 0, errors = 0;
   for (int i = TEST_CYCLES_START; i < TEST_CYCLES_END; ++i) {
-    void **pointers = calloc_func(sizeof(*pointers), 4096);
-    clock_t start;
+    for (int repeat = 0; repeat < TEST_CYCLES_REPEAT; ++repeat) {
+      void **pointers = calloc_func(sizeof(*pointers), 4096);
+      clock_t start;
 
-    /* malloc */
-    start = clock();
-    for (int j = 0; j < 4096; ++j) {
-      pointers[j] = malloc_func(i << 4);
-      if (i) {
-        if (!pointers[j])
-          ++errors;
-        else
-          ((char *)pointers[j])[0] = '1';
+      /* malloc */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        pointers[j] = malloc_func(i << 4);
+        if (i) {
+          if (!pointers[j])
+            ++errors;
+          else
+            ((char *)pointers[j])[0] = '1';
+        }
       }
-    }
-    clock_alloc += clock() - start;
+      clock_alloc += clock() - start;
 
-    /* realloc */
-    start = clock();
-    for (int j = 0; j < 4096; ++j) {
-      void *tmp = realloc_func(pointers[j], i << 5);
-      if (tmp) {
-        pointers[j] = tmp;
-        ((char *)pointers[j])[0] = '1';
-      } else if (i)
-        ++errors;
-    }
-    clock_realloc += clock() - start;
-
-    /* free (testing) */
-    start = clock();
-    for (int j = 0; j < 4096; ++j) {
-      free_func(pointers[j]);
-      pointers[j] = NULL;
-    }
-    clock_free += clock() - start;
-
-    /* calloc */
-    start = clock();
-    for (int j = 0; j < 4096; ++j) {
-      pointers[j] = calloc_func(16, i);
-      if (i) {
-        if (!pointers[j])
-          ++errors;
-        else
+      /* realloc */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        void *tmp = realloc_func(pointers[j], i << 5);
+        if (tmp) {
+          pointers[j] = tmp;
           ((char *)pointers[j])[0] = '1';
-      }
-    }
-    clock_calloc += clock() - start;
-
-    /* free (no test) */
-    for (int j = 0; j < 4096; ++j) {
-      free_func(pointers[j]);
-    }
-
-    /* facil.io use-case */
-    start = clock();
-    for (int j = 0; j < 4096; ++j) {
-      pointers[j] = malloc_func(i << 4);
-      if (i) {
-        if (!pointers[j])
+        } else if (i)
           ++errors;
-        else
-          ((char *)pointers[j])[0] = '1';
       }
-    }
-    for (int j = 0; j < 4096; ++j) {
-      free_func(pointers[j]);
-    }
-    fio_optimized += clock() - start;
+      clock_realloc += clock() - start;
 
-    /* facil.io use-case */
-    start = clock();
-    for (int j = 0; j < 4096; ++j) {
-      pointers[j] = malloc_func(i << 4);
-      if (i) {
-        if (!pointers[j])
-          ++errors;
-        else
-          ((char *)pointers[j])[0] = '1';
+      /* free (testing) */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        free_func(pointers[j]);
+        pointers[j] = NULL;
       }
-      free_func(pointers[j]);
-    }
-    fio_optimized2 += clock() - start;
+      clock_free += clock() - start;
 
-    free_func(pointers);
+      /* calloc */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        pointers[j] = calloc_func(16, i);
+        if (i) {
+          if (!pointers[j])
+            ++errors;
+          else
+            ((char *)pointers[j])[0] = '1';
+        }
+      }
+      clock_calloc += clock() - start;
+
+      /* free (no test) */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        free_func(pointers[j]);
+      }
+      clock_free2 += clock() - start;
+
+      /* facil.io use-case */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        pointers[j] = malloc_func(i << 4);
+        if (i) {
+          if (!pointers[j])
+            ++errors;
+          else
+            ((char *)pointers[j])[0] = '1';
+        }
+      }
+      for (int j = 0; j < 4096; ++j) {
+        free_func(pointers[j]);
+      }
+      fio_optimized += clock() - start;
+
+      /* facil.io use-case */
+      start = clock();
+      for (int j = 0; j < 4096; ++j) {
+        pointers[j] = malloc_func(i << 4);
+        if (i) {
+          if (!pointers[j])
+            ++errors;
+          else
+            ((char *)pointers[j])[0] = '1';
+        }
+        free_func(pointers[j]);
+      }
+      fio_optimized2 += clock() - start;
+
+      free_func(pointers);
+    }
   }
-  clock_alloc /= TEST_CYCLES_END - TEST_CYCLES_START;
-  clock_realloc /= TEST_CYCLES_END - TEST_CYCLES_START;
-  clock_free /= TEST_CYCLES_END - TEST_CYCLES_START;
-  clock_calloc /= TEST_CYCLES_END - TEST_CYCLES_START;
-  fio_optimized /= TEST_CYCLES_END - TEST_CYCLES_START;
-  fio_optimized2 /= TEST_CYCLES_END - TEST_CYCLES_START;
+  clock_alloc /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
+  clock_realloc /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
+  clock_free /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
+  clock_free2 /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
+  clock_calloc /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
+  fio_optimized /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
+  fio_optimized2 /= (TEST_CYCLES_END - TEST_CYCLES_START) * TEST_CYCLES_REPEAT;
   fprintf(stderr, "* Avrg. clock count for malloc: %zu\n", clock_alloc);
   fprintf(stderr, "* Avrg. clock count for calloc: %zu\n", clock_calloc);
   fprintf(stderr, "* Avrg. clock count for realloc: %zu\n", clock_realloc);
   fprintf(stderr, "* Avrg. clock count for free: %zu\n", clock_free);
+  fprintf(stderr, "* Avrg. clock count for free (re-cycle): %zu\n",
+          clock_free2);
   fprintf(stderr,
           "* Avrg. clock count for a facil.io use-case round"
           " (slow free): %zu\n",
@@ -121,7 +129,7 @@ static size_t test_mem_functions(void *(*malloc_func)(size_t),
           " (short life): %zu\n",
           fio_optimized2);
   fprintf(stderr, "* Failed allocations: %zu\n", errors);
-  return clock_alloc + clock_realloc + clock_free + clock_calloc;
+  return clock_alloc + clock_realloc + clock_free + clock_calloc + clock_free2;
 }
 
 void *test_system_malloc(void *ignr) {
