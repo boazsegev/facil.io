d52e3b67a882230bb955d5fce47c2cd8dd2ed7b7
Bo <Boaz@2be.co.il>
2018-05-01 11:22:18 -0400

    Fix for issue boazsegev/iodine#32

--- lib/facil/core/facil.c
+++ lib/facil/core/facil.c
@@ -28,6 +28,25 @@ Feel free to copy, use and enjoy according to the license provided.
 #endif
 
 /* *****************************************************************************
+Patch for OSX version < 10.12 from https://stackoverflow.com/a/9781275/4025095
+***************************************************************************** */
+#if defined(__MACH__) && !defined(CLOCK_REALTIME)
+#include <sys/time.h>
+#define CLOCK_REALTIME 0
+// clock_gettime is not implemented on older versions of OS X (< 10.12).
+// If implemented, CLOCK_REALTIME will have already been defined.
+static inline int clock_gettime(int /*clk_id*/, struct timespec *t) {
+  struct timeval now;
+  int rv = gettimeofday(&now, NULL);
+  if (rv)
+    return rv;
+  t->tv_sec = now.tv_sec;
+  t->tv_nsec = now.tv_usec * 1000;
+  return 0;
+}
+#endif
+
+/* *****************************************************************************
 Data Structures
 ***************************************************************************** */
 typedef struct ProtocolMetadata {
--- lib/facil/http/http.c
+++ lib/facil/http/http.c
@@ -22,6 +22,25 @@ Feel free to copy, use and enjoy according to the license provided.
 #include "fio_mem.h"
 
 /* *****************************************************************************
+Patch for OSX version < 10.12 from https://stackoverflow.com/a/9781275/4025095
+***************************************************************************** */
+#if defined(__MACH__) && !defined(CLOCK_REALTIME)
+#include <sys/time.h>
+#define CLOCK_REALTIME 0
+// clock_gettime is not implemented on older versions of OS X (< 10.12).
+// If implemented, CLOCK_REALTIME will have already been defined.
+static inline int clock_gettime(int /*clk_id*/, struct timespec *t) {
+  struct timeval now;
+  int rv = gettimeofday(&now, NULL);
+  if (rv)
+    return rv;
+  t->tv_sec = now.tv_sec;
+  t->tv_nsec = now.tv_usec * 1000;
+  return 0;
+}
+#endif
+
+/* *****************************************************************************
 Small Helpers
 ***************************************************************************** */
 static inline int hex2byte(uint8_t *dest, const uint8_t *source);
