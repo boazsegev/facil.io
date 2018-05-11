8f9ec887757bec555fd15cefa9997e63b1f9746a
Bo <bo@bowild.com>
2018-05-10 21:44:02 -0400

    possible include file ordering for FreeBSD

--- lib/facil/core/evio_kqueue.c
+++ lib/facil/core/evio_kqueue.c
@@ -12,6 +12,8 @@ Feel free to copy, use and enjoy according to the license provided.
 
 #ifdef EVIO_ENGINE_KQUEUE
 
+#include <sys/types.h>
+
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -20,13 +22,12 @@ Feel free to copy, use and enjoy according to the license provided.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/event.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
+#include <sys/event.h>
+#include <sys/time.h>
 /* *****************************************************************************
 Global data and system independant code
 ***************************************************************************** */
