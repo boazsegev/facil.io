c6906b1eddc7384e4cf9890877cbb33e2471821e
Bo <bo@bowild.com>
2018-06-06 12:07:54 +0200

    Try to support Typed Arrays

--- lib/facil/core/types/fiobj/fio_ary.h
+++ lib/facil/core/types/fiobj/fio_ary.h
@@ -7,6 +7,12 @@ License: MIT
 /**
  * A Dynamic Array for general use (void * pointers).
  *
+ * It's possible to switch from the default `void *` type to any type by
+ * defining the `FIO_ARY_TYPE` and `FIO_ARY_TYPE_COMPARE(a,b)` macros.
+ *
+ * However, this will effect ALL the arrays that share the same translation unit
+ * (the same *.c file).
+ *
  * The file was written to be compatible with C++ as well as C, hence some
  * pointer casting.
  *
@@ -16,7 +22,6 @@ fio_ary_s ary;                   // a container can be placed on the stack.
 fio_ary_new(&ary);               // initialize the container
 fio_ary_push(&ary, (void*)1 );   // add / remove / read data...
 fio_ary_free(&ary)               // free any resources, not the container.
-
 
  */
 #define H_FIO_ARRAY_H
@@ -28,6 +33,20 @@ fio_ary_free(&ary)               // free any resources, not the container.
 #include <string.h>
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
+
+/**
+ * Both FIO_ARY_TYPE, FIO_ARY_TYPE_INVALID and FIO_ARY_TYPE_COMPARE must be
+ * defined to change default behavior.
+ */
+#if !defined(FIO_ARY_TYPE) || !defined(FIO_ARY_TYPE_INVALID) ||                \
+    !defined(FIO_ARY_TYPE_COMPARE)
+#undef FIO_ARY_TYPE
+#undef FIO_ARY_TYPE_INVALID
+#undef FIO_ARY_TYPE_COMPARE
+#define FIO_ARY_TYPE void *
+#define FIO_ARY_TYPE_INVALID NULL
+#define FIO_ARY_TYPE_COMPARE(a, b) ((a) == (b))
 #endif
 
 #ifndef FIO_FUNC
@@ -42,7 +61,7 @@ typedef struct fio_ary_s {
   size_t start;
   size_t end;
   size_t capa;
-  void **arry;
+  FIO_ARY_TYPE *arry;
 } fio_ary_s;
 
 /** this value can be used for lazy initialization. */
@@ -65,13 +84,13 @@ FIO_FUNC inline size_t fio_ary_count(fio_ary_s *ary);
 FIO_FUNC inline size_t fio_ary_capa(fio_ary_s *ary);
 
 /**
- * Returns the object placed in the Array, if any. Returns NULL if no data or if
- * the index is out of bounds.
+ * Returns the object placed in the Array, if any. Returns FIO_ARY_TYPE_INVALID
+ * if no data or if the index is out of bounds.
  *
  * Negative values are retrived from the end of the array. i.e., `-1`
  * is the last item.
  */
-FIO_FUNC inline void *fio_ary_index(fio_ary_s *ary, intptr_t pos);
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_index(fio_ary_s *ary, intptr_t pos);
 
 /** alias for `fiobj_ary_index` */
 #define fio_ary_entry(a, p) fiobj_ary_index((a), (p))
@@ -83,25 +102,35 @@ FIO_FUNC inline void *fio_ary_index(fio_ary_s *ary, intptr_t pos);
  *
  * If an error occurs, the same data passed to the function is returned.
  */
-FIO_FUNC inline void *fio_ary_set(fio_ary_s *ary, void *data, intptr_t pos);
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_set(fio_ary_s *ary, FIO_ARY_TYPE data,
+                                         intptr_t pos);
 
 /**
  * Pushes an object to the end of the Array. Returns -1 on error.
  */
-FIO_FUNC inline int fio_ary_push(fio_ary_s *ary, void *data);
+FIO_FUNC inline int fio_ary_push(fio_ary_s *ary, FIO_ARY_TYPE data);
 
 /** Pops an object from the end of the Array. */
-FIO_FUNC inline void *fio_ary_pop(fio_ary_s *ary);
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_pop(fio_ary_s *ary);
 
 /**
  * Unshifts an object to the beginning of the Array. Returns -1 on error.
  *
  * This could be expensive, causing `memmove`.
  */
-FIO_FUNC inline int fio_ary_unshift(fio_ary_s *ary, void *data);
+FIO_FUNC inline int fio_ary_unshift(fio_ary_s *ary, FIO_ARY_TYPE data);
 
 /** Shifts an object from the beginning of the Array. */
-FIO_FUNC inline void *fio_ary_shift(fio_ary_s *ary);
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_shift(fio_ary_s *ary);
+
+/**
+ * Removes an object from the, MOVING all the objects to prevent "holes" in the
+ * data.
+ *
+ * Returns -1 if the object wasn't found or 0 if the object was successfully
+ * removed.
+ */
+FIO_FUNC inline int fio_ary_remove(fio_ary_s *ary, FIO_ARY_TYPE data);
 
 /**
  * Iteration using a callback for each entry in the array.
@@ -115,11 +144,11 @@ FIO_FUNC inline void *fio_ary_shift(fio_ary_s *ary);
  * the starting point.
  */
 FIO_FUNC inline size_t fio_ary_each(fio_ary_s *ary, size_t start_at,
-                                    int (*task)(void *pt, void *arg),
+                                    int (*task)(FIO_ARY_TYPE pt, void *arg),
                                     void *arg);
 /**
- * Removes any NULL *pointers* from an Array, keeping all other data in the
- * array.
+ * Removes any FIO_ARY_TYPE_INVALID object from an Array (NULL pointers by
+ * default), keeping all other data in the array.
  *
  * This action is O(n) where n in the length of the array.
  * It could get expensive.
@@ -140,7 +169,7 @@ FIO_FUNC inline void fio_ary_compact(fio_ary_s *ary);
          (++pos.i))
 struct fio_ary_pos_for_loop_s {
   unsigned long i;
-  void *obj;
+  FIO_ARY_TYPE obj;
 };
 
 /* *****************************************************************************
@@ -150,7 +179,7 @@ Array creation API
 FIO_FUNC inline void fio_ary_new(fio_ary_s *ary, size_t capa) {
   if (!capa)
     capa = 32;
-  *ary = (fio_ary_s){.arry = (void **)malloc(capa * sizeof(*ary->arry)),
+  *ary = (fio_ary_s){.arry = (FIO_ARY_TYPE *)malloc(capa * sizeof(*ary->arry)),
                      .capa = capa};
   if (!ary->arry) {
     perror("ERROR: facil.io dynamic array couldn't be allocated");
@@ -202,7 +231,8 @@ FIO_FUNC void fio_ary_getmem(fio_ary_s *ary, intptr_t needed) {
 
   /* we assume memory allocation works. it's better to crash than to continue
    * living without memory... besides, malloc is optimistic these days. */
-  ary->arry = (void **)realloc(ary->arry, updated_capa * sizeof(*ary->arry));
+  ary->arry =
+      (FIO_ARY_TYPE *)realloc(ary->arry, updated_capa * sizeof(*ary->arry));
   ary->capa = updated_capa;
   if (!ary->arry) {
     perror("ERROR: facil.io dynamic array couldn't be reallocated");
@@ -246,20 +276,20 @@ FIO_FUNC inline size_t fio_ary_capa(fio_ary_s *ary) { return ary->capa; }
  * Negative values are retrived from the end of the array. i.e., `-1`
  * is the last item.
  */
-FIO_FUNC inline void *fio_ary_index(fio_ary_s *ary, intptr_t pos) {
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_index(fio_ary_s *ary, intptr_t pos) {
   if (!ary || !ary->arry)
-    return NULL;
+    return FIO_ARY_TYPE_INVALID;
   /* position is relative to `start`*/
   if (pos >= 0) {
     pos = pos + ary->start;
     if ((size_t)pos >= ary->end)
-      return NULL;
+      return FIO_ARY_TYPE_INVALID;
     return ary->arry[pos];
   }
   /* position is relative to `end`*/
   pos = (intptr_t)ary->end + pos;
   if (pos < 0 || (size_t)pos < ary->start)
-    return NULL;
+    return FIO_ARY_TYPE_INVALID;
   return ary->arry[pos];
 }
 
@@ -273,11 +303,12 @@ FIO_FUNC inline void *fio_ary_index(fio_ary_s *ary, intptr_t pos) {
  *
  * If an error occurs, the same data passed to the function is returned.
  */
-FIO_FUNC inline void *fio_ary_set(fio_ary_s *ary, void *data, intptr_t pos) {
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_set(fio_ary_s *ary, FIO_ARY_TYPE data,
+                                         intptr_t pos) {
   if (!ary->arry) {
     fio_ary_new(ary, 0);
   }
-  void *old = NULL;
+  FIO_ARY_TYPE old = FIO_ARY_TYPE_INVALID;
   /* test for memory and request memory if missing, promises valid bounds. */
   if (pos >= 0) {
     if ((size_t)pos + ary->start >= ary->capa)
@@ -290,13 +321,13 @@ FIO_FUNC inline void *fio_ary_set(fio_ary_s *ary, void *data, intptr_t pos) {
     pos = pos + ary->start;
     /* initialize empty spaces, if any, setting new boundries */
     while ((size_t)pos >= ary->end)
-      ary->arry[(ary->end)++] = NULL;
+      ary->arry[(ary->end)++] = FIO_ARY_TYPE_INVALID;
   } else {
     /* position relative to end */
     pos = pos + (intptr_t)ary->end;
     /* initialize empty spaces, if any, setting new boundries */
     while (ary->start > (size_t)pos)
-      ary->arry[--(ary->start)] = NULL;
+      ary->arry[--(ary->start)] = FIO_ARY_TYPE_INVALID;
   }
 
   /* check for an existing object and set new objects */
@@ -313,7 +344,7 @@ Array push / shift API
 /**
  * Pushes an object to the end of the Array. Returns -1 on error.
  */
-FIO_FUNC inline int fio_ary_push(fio_ary_s *ary, void *data) {
+FIO_FUNC inline int fio_ary_push(fio_ary_s *ary, FIO_ARY_TYPE data) {
   if (!ary->arry)
     fio_ary_new(ary, 0);
   else if (ary->capa <= ary->end)
@@ -324,9 +355,9 @@ FIO_FUNC inline int fio_ary_push(fio_ary_s *ary, void *data) {
 }
 
 /** Pops an object from the end of the Array. */
-FIO_FUNC inline void *fio_ary_pop(fio_ary_s *ary) {
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_pop(fio_ary_s *ary) {
   if (!ary || ary->start == ary->end)
-    return NULL;
+    return FIO_ARY_TYPE_INVALID;
   ary->end -= 1;
   return ary->arry[ary->end];
 }
@@ -336,7 +367,7 @@ FIO_FUNC inline void *fio_ary_pop(fio_ary_s *ary) {
  *
  * This could be expensive, causing `memmove`.
  */
-FIO_FUNC inline int fio_ary_unshift(fio_ary_s *ary, void *data) {
+FIO_FUNC inline int fio_ary_unshift(fio_ary_s *ary, FIO_ARY_TYPE data) {
   if (!ary->arry)
     fio_ary_new(ary, 0);
   else if (!ary->start)
@@ -347,9 +378,9 @@ FIO_FUNC inline int fio_ary_unshift(fio_ary_s *ary, void *data) {
 }
 
 /** Shifts an object from the beginning of the Array. */
-FIO_FUNC inline void *fio_ary_shift(fio_ary_s *ary) {
+FIO_FUNC inline FIO_ARY_TYPE fio_ary_shift(fio_ary_s *ary) {
   if (!ary || ary->start == ary->end)
-    return NULL;
+    return FIO_ARY_TYPE_INVALID;
 #ifdef __cplusplus
   const size_t pos = ary->start;
 #else
@@ -357,6 +388,31 @@ FIO_FUNC inline void *fio_ary_shift(fio_ary_s *ary) {
 #endif
   ary->start += 1;
   return ary->arry[pos];
+}
+
+/**
+ * Removes an object from the, MOVING all the other objects to prevent "holes"
+ * in the data.
+ *
+ * Returns -1 if the object wasn't found or 0 if the object was successfully
+ * removed.
+ */
+FIO_FUNC inline int fio_ary_remove(fio_ary_s *ary, FIO_ARY_TYPE data) {
+  if (!ary || ary->start == ary->end || !ary->arry) {
+    return -1;
+  }
+  size_t pos = ary->start;
+  const size_t end = ary->end;
+  while (pos < end && !FIO_ARY_TYPE_COMPARE(data, ary->arry[pos])) {
+    ++pos;
+  }
+  if (pos == end)
+    return -1;
+  --ary->end;
+  while (pos < end - 1) {
+    ary->arry[pos] = ary->arry[pos + 1];
+  }
+  return 0;
 }
 
 /**
@@ -371,7 +427,7 @@ FIO_FUNC inline void *fio_ary_shift(fio_ary_s *ary) {
  * the starting point.
  */
 FIO_FUNC inline size_t fio_ary_each(fio_ary_s *ary, size_t start_at,
-                                    int (*task)(void *data, void *arg),
+                                    int (*task)(FIO_ARY_TYPE data, void *arg),
                                     void *arg) {
   if (!ary || ary->start == ary->end)
     return 0;
@@ -387,8 +443,8 @@ Array compacting (untested)
 ***************************************************************************** */
 
 /**
- * Removes any NULL *pointers* from an Array, keeping all other data in the
- * array.
+ * Removes any FIO_ARY_TYPE_INVALID  *pointers* from an Array, keeping all other
+ * data in the array.
  *
  * This action is O(n) where n in the length of the array.
  * It could get expensive.
@@ -396,9 +452,9 @@ Array compacting (untested)
 FIO_FUNC inline void fio_ary_compact(fio_ary_s *ary) {
   if (!ary || ary->start == ary->end)
     return;
-  register void **pos = ary->arry + ary->start;
-  register void **reader = ary->arry + ary->start;
-  register void **stop = ary->arry + ary->end;
+  register FIO_ARY_TYPE *pos = ary->arry + ary->start;
+  register FIO_ARY_TYPE *reader = ary->arry + ary->start;
+  register FIO_ARY_TYPE *stop = ary->arry + ary->end;
   while (reader < stop) {
     if (*reader) {
       *pos = *reader;
