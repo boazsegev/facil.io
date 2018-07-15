1886ce78430f346da2d5cf1e9bfebf018f33380d
Bo <bo@bowild.com>
2018-07-15 14:01:12 -0400

    move roundtrip test to new API

--- tests/pubsub_roundtrip.c
+++ tests/pubsub_roundtrip.c
@@ -1,16 +1,15 @@
 #include "facil.h"
-#include "pubsub.h"
 #include "redis_engine.h"
 
 static void reporter_subscribe(const pubsub_engine_s *eng, FIOBJ channel,
-                               pubsub_match_fn match) {
+                               facil_match_fn match) {
   fprintf(stderr, "(%u) + subscribing to %s\n", getpid(),
           fiobj_obj2cstr(channel).data);
   (void)eng;
   (void)match;
 }
 static void reporter_unsubscribe(const pubsub_engine_s *eng, FIOBJ channel,
-                                 pubsub_match_fn match) {
+                                 facil_match_fn match) {
   fprintf(stderr, "(%u) - unsubscribing to %s\n", getpid(),
           fiobj_obj2cstr(channel).data);
   (void)eng;
@@ -31,18 +30,15 @@ pubsub_engine_s REPORTER = {
     .publish = reporter_publish,
 };
 
-void my_on_message(pubsub_message_s *msg) {
+void my_on_message(facil_msg_s *msg) {
   fio_cstr_s s = fiobj_obj2cstr(msg->channel);
-  if (FIOBJ_TYPE(msg->message) == FIOBJ_T_STRING) {
+  if (FIOBJ_TYPE(msg->msg) == FIOBJ_T_STRING) {
     fprintf(stderr, "Got message from %s: %s\n", s.data,
-            fiobj_obj2cstr(msg->message).data);
+            fiobj_obj2cstr(msg->msg).data);
   } else {
     fprintf(stderr, "Got message from %s, with subscription %p\n", s.data,
-            (void *)fiobj_obj2num(msg->message));
-    pubsub_sub_pt sub =
-        pubsub_find_sub(.channel = msg->channel, .on_message = my_on_message,
-                        .udata1 = msg->udata1, .udata2 = msg->udata2);
-    pubsub_unsubscribe(sub);
+            (void *)fiobj_obj2num(msg->msg));
+    facil_unsubscribe((void *)fiobj_obj2num(msg->msg));
   }
 }
 
@@ -52,35 +48,35 @@ void perfrom_sub(void *a) {
   } else {
     FIOBJ ch = fiobj_str_new("my channel", 10);
     FIOBJ msg = fiobj_num_new(
-        (intptr_t)pubsub_subscribe(.channel = ch, .on_message = my_on_message,
-                                   .udata1 = a, .udata2 = NULL,
-                                   .match = PUBSUB_MATCH_GLOB));
-    pubsub_publish(.channel = ch, .message = ch);
-    pubsub_publish(.channel = ch, .message = msg);
+        (intptr_t)facil_subscribe(.channel = ch, .on_message = my_on_message,
+                                  .udata1 = a, .udata2 = NULL,
+                                  .match = FACIL_MATCH_GLOB));
+    facil_publish(.channel = ch, .message = ch);
+    facil_publish(.channel = ch, .message = msg);
     fiobj_free(msg);
     fiobj_free(ch);
   }
 }
 
-void watch_all_messages(pubsub_message_s *msg) {
+void watch_all_messages(facil_msg_s *msg) {
   fio_cstr_s s = fiobj_obj2cstr(msg->channel);
-  if (FIOBJ_TYPE(msg->message) == FIOBJ_T_STRING) {
+  if (FIOBJ_TYPE(msg->msg) == FIOBJ_T_STRING) {
     fprintf(stderr, "Got message from %s: %s\n", s.data,
-            fiobj_obj2cstr(msg->message).data);
+            fiobj_obj2cstr(msg->msg).data);
   } else {
     fprintf(stderr, "Got message from %s, with subscription %p\n", s.data,
-            (void *)fiobj_obj2num(msg->message));
+            (void *)fiobj_obj2num(msg->msg));
   }
 }
 int main(void) {
   // PUBSUB_DEFAULT_ENGINE = redis_engine_create(.address = "localhost");
   FIOBJ ch = fiobj_str_new("*", 1);
-  pubsub_subscribe(.channel = ch, .on_message = watch_all_messages,
-                   .udata1 = NULL, .udata2 = NULL, .match = PUBSUB_MATCH_GLOB);
+  facil_subscribe(.channel = ch, .on_message = watch_all_messages,
+                  .udata1 = NULL, .udata2 = NULL, .match = FACIL_MATCH_GLOB);
   fiobj_free(ch);
-  pubsub_engine_register(&REPORTER);
+  facil_pubsub_attach(&REPORTER);
   facil_run_every(1000, 4, perfrom_sub, NULL, NULL);
   facil_run(.threads = 4, .processes = 4);
-  pubsub_engine_deregister(&REPORTER);
+  facil_pubsub_detach(&REPORTER);
   return 0;
 }
