e67eb2db3641b991dd789c97d1e715f3a97ddcb0
Bo <bo@bowild.com>
2018-09-14 15:35:34 -0400

    minorities

--- lib/facil/fio.h
+++ lib/facil/fio.h
@@ -1772,13 +1772,14 @@ FIO_FUNC inline void fio_throttle_thread(size_t nano_sec);
    (((i)&0xFF000000000000ULL) >> 40) | (((i)&0xFF00000000000000ULL) >> 56))
 #endif
 
+/* Note: using BIG_ENDIAN invokes false positives on some systems */
 #if (defined(__BIG_ENDIAN__) && __BIG_ENDIAN__) ||                             \
     (defined(__LITTLE_ENDIAN__) &&                                             \
      !__LITTLE_ENDIAN__) || /*(defined(BIG_ENDIAN) && BIG_ENDIAN) ||   */      \
-    (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+    (defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__))
 #define __BIG_ENDIAN__ 1
 #warning Byte Order is big endian!
-#elif !defined(__BIG_ENDIAN__) && !defined(BIG_ENDIAN) &&                      \
+#elif !defined(__BIG_ENDIAN__) && /* !defined(BIG_ENDIAN) &&  */               \
     !defined(__BYTE_ORDER__) && !defined(__LITTLE_ENDIAN__)
 #error Couldn't detect byte order on this system.
 #endif
