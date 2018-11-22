a2aece8f21ff9ccabe8f30aa089b56a908f91025
Bo <bo@bowild.com>
2018-11-22 02:55:10 -0500

    tweaks

--- lib/facil/http/http.c
+++ lib/facil/http/http.c
@@ -19,27 +19,6 @@ Feel free to copy, use and enjoy according to the license provided.
 #include <unistd.h>
 
 /* *****************************************************************************
-Patch for OSX version < 10.12 from https://stackoverflow.com/a/9781275/4025095
-***************************************************************************** */
-#if defined(__MACH__) && !defined(CLOCK_REALTIME)
-#include <sys/time.h>
-#define CLOCK_REALTIME 0
-#define clock_gettime patch_clock_gettime
-// clock_gettime is not implemented on older versions of OS X (< 10.12).
-// If implemented, CLOCK_REALTIME will have already been defined.
-static inline int patch_clock_gettime(int clk_id, struct timespec *t) {
-  struct timeval now;
-  int rv = gettimeofday(&now, NULL);
-  if (rv)
-    return rv;
-  t->tv_sec = now.tv_sec;
-  t->tv_nsec = now.tv_usec * 1000;
-  return 0;
-  (void)clk_id;
-}
-#endif
-
-/* *****************************************************************************
 SSL/TLS patch
 ***************************************************************************** */
 
@@ -2097,9 +2076,9 @@ void http_write_log(http_s *h) {
   fiobj_str_join(l, h->version);
   fiobj_str_write(l, "\" ", 2);
   if (bytes_sent > 0) {
-    fiobj_str_join(l, fiobj_num_tmp(h->status));
+    fiobj_str_write_i(l, h->status);
     fiobj_str_write(l, " ", 1);
-    fiobj_str_join(l, fiobj_num_tmp(bytes_sent));
+    fiobj_str_write_i(l, bytes_sent);
     fiobj_str_write(l, "b ", 2);
   } else {
     fiobj_str_join(l, fiobj_num_tmp(h->status));
@@ -2108,7 +2087,7 @@ void http_write_log(http_s *h) {
 
   bytes_sent = ((end.tv_sec - start.tv_sec) * 1000) +
                ((end.tv_nsec - start.tv_nsec) / 1000000);
-  fiobj_str_join(l, fiobj_num_tmp(bytes_sent));
+  fiobj_str_write_i(l, bytes_sent);
   fiobj_str_write(l, "ms\r\n", 4);
 
   buff = fiobj_obj2cstr(l);
