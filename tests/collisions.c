a59d314f17e212fb71dc61d53858ec38c430e972
Bo <bo@bowild.com>
2019-06-03 01:37:53 +0300

    Update collision tests

--- tests/collisions.c
+++ tests/collisions.c
@@ -6,7 +6,6 @@ Feel free to copy, use and enjoy according to the license provided.
 */
 #include <fio.h>
 
-#define FIO_STR_NAME fio_str
 #define FIO_CLI 1
 #define FIO_RAND 1
 #define FIO_LOG 1
@@ -23,6 +22,8 @@ Feel free to copy, use and enjoy according to the license provided.
 /* *****************************************************************************
 State machine and types
 ***************************************************************************** */
+#define FIO_STR_NAME fio_str
+#include <fio-stl.h>
 
 static uint8_t print_flag = 1;
 
@@ -80,6 +81,7 @@ static void initialize_hash_names(void);
 static void print_hash_names(void);
 static char *hash_name(hashing_func_fn fn);
 static void cleanup(void);
+static void find_bit_collisions(hashing_func_fn fn, size_t count, uint8_t bits);
 
 int main(int argc, char const *argv[]) {
   // FIO_LOG_LEVEL = FIO_LOG_LEVEL_DEBUG;
@@ -88,16 +90,46 @@ int main(int argc, char const *argv[]) {
   initialize_hash_names();
   if (fio_cli_get("-t")) {
     fio_str_s tmp = FIO_STR_INIT_STATIC(fio_cli_get("-t"));
-    hashing_func_fn h = hash_name_find(&hash_names, fio_str_hash(&tmp), NULL);
+    hashing_func_fn h =
+        hash_name_find(&hash_names, fio_str_hash(&tmp, 0), NULL);
     if (h) {
       test_hash_function(h);
+      find_bit_collisions(h, 8, fio_cli_get_i("-b"));
     } else {
       FIO_LOG_ERROR("Test function %s unknown.", tmp.data);
       fprintf(stderr, "Try any of the following:\n");
       print_hash_names();
     }
   } else {
-    FIO_MAP_EACH(&hash_names, pos) { test_hash_function(pos->obj); }
+    uint8_t skip = 0;
+    FIO_MAP_EACH(&hash_names, pos) {
+      /* skip collision tester */
+      if (!skip) {
+        ++skip;
+        continue;
+      }
+      test_hash_function(pos->obj);
+      if (skip < 2) {
+        /* don't brute force the counter. It might never happen... */
+        ++skip;
+        continue;
+      }
+      if (fio_cli_get_i("-b")) {
+        if (fio_cli_get_i("-b") < 0) {
+          fprintf(
+              stderr,
+              "ERROR: brute force bits value can't be less than zero (%zu).\n",
+              (size_t)fio_cli_get_i("-b"));
+        } else if (fio_cli_get_i("-b") > 20) {
+          fprintf(
+              stderr,
+              "ERROR: brute force bits value above testing limit (%zu > 20).\n",
+              (size_t)fio_cli_get_i("-b"));
+        } else {
+          find_bit_collisions(pos->obj, 8, fio_cli_get_i("-b"));
+        }
+      }
+    }
   }
   cleanup();
   return 0;
@@ -122,6 +154,7 @@ static void initialize_cli(int argc, char const *argv[]) {
       FIO_CLI_STRING(
           "-dictionary -d a text file containing words separated by an "
           "EOL marker."),
+      FIO_CLI_INT("-bits -b number of collision bits in a brute force attack."),
       FIO_CLI_BOOL("-v make output more verbouse (debug mode)"));
   if (fio_cli_get_bool("-v"))
     FIO_LOG_LEVEL = FIO_LOG_LEVEL_DEBUG;
@@ -132,9 +165,7 @@ static void initialize_cli(int argc, char const *argv[]) {
 Dictionary management
 ***************************************************************************** */
 
-static void add_bad_words(void);
 static void load_words(void) {
-  add_bad_words();
   fio_str_s filename = FIO_STR_INIT;
   fio_str_s data = FIO_STR_INIT;
 
@@ -288,6 +319,12 @@ inline FIO_FUNC uintptr_t risky(char *data, size_t len) {
   return fio_risky_hash(data, len, 0);
 }
 
+inline FIO_FUNC uintptr_t collision_promise(char *data, size_t len) {
+  return ((0x55555555ULL << 31) << 1) | 0x55555555ULL;
+  (void)data;
+  (void)len;
+}
+
 /* *****************************************************************************
 Hash setup and testing...
 ***************************************************************************** */
@@ -296,6 +333,7 @@ struct hash_fn_names_s {
   char *name;
   hashing_func_fn fn;
 } hash_fn_list[] = {
+    {"collider", collision_promise},
     {"counter (no hash, RAM access test)", counter},
 #ifdef H_FACIL_IO_H
     {"siphash13", siphash13},
@@ -313,7 +351,7 @@ struct hash_fn_names_s {
 static void initialize_hash_names(void) {
   for (size_t i = 0; hash_fn_list[i].name; ++i) {
     fio_str_s tmp = FIO_STR_INIT_STATIC(hash_fn_list[i].name);
-    hash_name_insert(&hash_names, fio_str_hash(&tmp), hash_fn_list[i].fn);
+    hash_name_insert(&hash_names, fio_str_hash(&tmp, 0), hash_fn_list[i].fn);
     FIO_LOG_DEBUG("Registered %s hashing function.\n\t\t(%zu registered)",
                   hash_fn_list[i].name, hash_name_count(&hash_names));
   }
@@ -406,265 +444,8 @@ static void test_hash_function(hashing_func_fn h) {
   collisions_destroy(&c);
 }
 
-/* *****************************************************************************
-Finsing a mod64 inverse
-See: https://lemire.me/blog/2017/09/18/computing-the-inverse-of-odd-integers/
-***************************************************************************** */
-
-/* will return `inv` if `inv` is inverse of `n` */
-static uint64_t inverse64_test(uint64_t n, uint64_t inv) {
-  uint64_t result = inv * (2 - (n * inv));
-  return result;
-}
-
-static uint64_t inverse64(uint64_t x) {
-  uint64_t y = (3 * x) ^ 2;
-  y = inverse64_test(x, y);
-  y = inverse64_test(x, y);
-  y = inverse64_test(x, y);
-  y = inverse64_test(x, y);
-  if (FIO_LOG_LEVEL >= FIO_LOG_LEVEL_DEBUG) {
-    char buff[64];
-    fio_str_s t = FIO_STR_INIT;
-    fio_str_write(&t, "\n\t\tinverse for:\t", 16);
-    fio_str_write(&t, buff, fio_ltoa(buff, x, 16));
-    fio_str_write(&t, "\n\t\tis:\t\t\t", 8);
-    fio_str_write(&t, buff, fio_ltoa(buff, y, 16));
-    fio_str_write(&t, "\n\t\tsanity inverse test: 1==", 27);
-    fio_str_write_i(&t, x * y);
-    FIO_LOG_DEBUG("%s", fio_str_data(&t));
-  }
-
-  return y;
-}
-
-/* *****************************************************************************
-Hash Breaking Word Workshop
-***************************************************************************** */
-
-/**
- * Attacking 8 byte words, which follow this code path:
- *      h64 = seed + PRIME64_5;
- *      h64 += len; // len == 8
- *      if (p + 4 <= bEnd) {
- *        h64 ^= (U64)(XXH_get32bits(p)) * PRIME64_1;
- *        h64 = XXH_rotl64(h64, 23) * PRIME64_2 + PRIME64_3;
- *        p += 4;
- *      }
- *
- *      while (p < bEnd) {
- *        h64 ^= (*p) * PRIME64_5;
- *        h64 = XXH_rotl64(h64, 11) * PRIME64_1;
- *        p++;
- *      }
- *
- *      h64 ^= h64 >> 33;
- *      h64 *= PRIME64_2;
- *      h64 ^= h64 >> 29;
- *      h64 *= PRIME64_3;
- *      h64 ^= h64 >> 32;
- */
-
-FIO_FUNC void attack_xxhash2(void) {
-  /* POC - forcing XXHash to return seed only data (here, seed = 0) */
-  const uint64_t PRIME64_1 = 11400714785074694791ULL;
-  const uint64_t PRIME64_2 = 14029467366897019727ULL;
-  // const uint64_t PRIME64_3 = 1609587929392839161ULL;
-  // const uint64_t PRIME64_4 = 9650029242287828579ULL;
-  // const uint64_t PRIME64_5 = 2870177450012600261ULL;
-  const uint64_t PRIME64_1_INV = inverse64(PRIME64_1);
-  const uint64_t PRIME64_2_INV = inverse64(PRIME64_2);
-  // const uint64_t PRIME64_3_INV = inverse64(PRIME64_3);
-  // const uint64_t PRIME64_4_INV = inverse64(PRIME64_4);
-  // const uint64_t PRIME64_5_INV = inverse64(PRIME64_5);
-  const uint64_t seed_manipulation[4] = {PRIME64_1 + PRIME64_2, PRIME64_2, 0,
-                                         -PRIME64_1};
-  uint64_t v[4] = {0, 0, 0, 0};
-  /* attack v *= PRIME64_1 */
-  v[0] = v[0] * PRIME64_1_INV;
-  v[1] = v[1] * PRIME64_1_INV;
-  v[2] = v[2] * PRIME64_1_INV;
-  v[3] = v[3] * PRIME64_1_INV;
-  /* attack v = XXH_rotl64(v, 31) */
-  v[0] = (v[0] >> 31) | (v[0] << (64 - 31));
-  v[1] = (v[1] >> 31) | (v[1] << (64 - 31));
-  v[2] = (v[2] >> 31) | (v[2] << (64 - 31));
-  v[3] = (v[3] >> 31) | (v[3] << (64 - 31));
-  /* attack seed manipulation */
-  v[0] = v[0] - seed_manipulation[0];
-  v[1] = v[1] - seed_manipulation[1];
-  v[2] = v[2] - seed_manipulation[2];
-  v[3] = v[3] - seed_manipulation[3];
-  /* attack v += XXH_get64bits(p) * PRIME64_2 */
-  v[0] = v[0] * PRIME64_2_INV;
-  v[1] = v[1] * PRIME64_2_INV;
-  v[2] = v[2] * PRIME64_2_INV;
-  v[3] = v[3] * PRIME64_2_INV;
-  uint64_t seed_data = XXH64(v, 32, 0);
-  if (seed_data == 0)
-    fprintf(stderr, "XXHash seed data extracted for seed == 0!\n");
-  else
-    fprintf(stderr, "Seed extraction failed %llu\n", seed_data);
-}
-
-FIO_FUNC void attack_xxhash(void) {
-  /* POC - forcing XXHash to return seed only data (here, seed = 0) */
-  const uint64_t PRIME64_1 = 11400714785074694791ULL;
-  const uint64_t PRIME64_2 = 14029467366897019727ULL;
-  const uint64_t PRIME64_3 = 1609587929392839161ULL;
-  const uint64_t PRIME64_4 = 9650029242287828579ULL;
-  const uint64_t PRIME64_2_INV = 0x0BA79078168D4BAFULL;
-  const uint64_t seed_manipulation[4] = {PRIME64_1 + PRIME64_2, PRIME64_2, 0,
-                                         -PRIME64_1};
-  uint64_t v[4] = {0, 0, 0, 0};
-  /* attack seed manipulation */
-  v[0] = v[0] - seed_manipulation[0];
-  v[1] = v[1] - seed_manipulation[1];
-  v[2] = v[2] - seed_manipulation[2];
-  v[3] = v[3] - seed_manipulation[3];
-  /* attack v += XXH_get64bits(p) * PRIME64_2 */
-  v[0] = v[0] * PRIME64_2_INV;
-  v[1] = v[1] * PRIME64_2_INV;
-  v[2] = v[2] * PRIME64_2_INV;
-  v[3] = v[3] * PRIME64_2_INV;
-
-  uint64_t seed = 2870177450012600261ULL;
-  uint64_t expected_seed;
-
-  /* I didn't work out how to extract the seeed from this part */
-  expected_seed = fio_lrot(seed, 1) + fio_lrot(seed, 7) + fio_lrot(seed, 12) +
-                  fio_lrot(seed, 18);
-  uint64_t tmp = seed * PRIME64_2;
-  tmp = fio_lrot(tmp, 31);
-  tmp *= PRIME64_1;
-  expected_seed ^= tmp;
-  expected_seed = expected_seed * PRIME64_1 + PRIME64_4;
-  expected_seed ^= tmp;
-  expected_seed = expected_seed * PRIME64_1 + PRIME64_4;
-  expected_seed ^= tmp;
-  expected_seed = expected_seed * PRIME64_1 + PRIME64_4;
-  expected_seed ^= tmp;
-  expected_seed = expected_seed * PRIME64_1 + PRIME64_4;
-  expected_seed += 32;
-  expected_seed ^= expected_seed >> 33;
-  expected_seed *= PRIME64_2;
-  expected_seed ^= expected_seed >> 29;
-  expected_seed *= PRIME64_3;
-  expected_seed ^= expected_seed >> 32;
-
-  uint64_t seed_data = XXH64(v, 32, 0);
-  if (seed_data == expected_seed)
-    fprintf(stderr, "XXHash extraxted seed data matches expectations!\n");
-  else
-    fprintf(stderr, "Seed extraction failed %llu\n", seed_data);
-  //   char b[128] = {0};
-  //   fio_ltoa(b, v[0], 16);
-  //   fio_ltoa(b + 32, v[1], 16);
-  //   fio_ltoa(b + 64, v[2], 16);
-  //   fio_ltoa(b + 96, v[3], 16);
-  //   fprintf(stderr, "Message was:\n%s\n%s\n%s\n%s\n", b, b + 32, b + 64, b +
-  //           96);
-  // Output (message):
-  //    0xFB9FE7DB392000B6
-  //    0xFFFFFFFFFFFFFFFF
-  //    0x0000000000000000
-  //    0x04601824C6DFFF49
-}
-
-/**
- * Attacking 64 byte messages where the last 32 bytes are the same and the first
- * 32 bytes use rotating 8 byte words. This is attcking the following part in
- * the code:
- *
- *    U64 v1 = seed + PRIME64_1 + PRIME64_2;
- *    U64 v2 = seed + PRIME64_2;
- *    U64 v3 = seed + 0;
- *    U64 v4 = seed - PRIME64_1;
- *
- *    do {
- *      v1 += XXH_get64bits(p) * PRIME64_2;
- *      p += 8;
- *      v1 = XXH_rotl64(v1, 31);
- *      v1 *= PRIME64_1;
- *      //... v2, v3, v4 same;
- *    } while (p <= limit);
- *
- *    h64 = XXH_rotl64(v1, 1) + XXH_rotl64(v2, 7) + XXH_rotl64(v3, 12) +
- *          XXH_rotl64(v4, 18);
- */
-
-FIO_FUNC void add_bad4xxhash(void) {
-  attack_xxhash();
-  const uint64_t PRIME64_1 = 11400714785074694791ULL;
-  const uint64_t PRIME64_2 = 14029467366897019727ULL;
-  const uint64_t PRIME64_1_INV = inverse64(PRIME64_1);
-  const uint64_t PRIME64_2_INV = inverse64(PRIME64_2);
-  const uint64_t seed_manipulation[4] = {PRIME64_1 + PRIME64_2, PRIME64_2, 0,
-                                         -PRIME64_1};
-
-  uint64_t rotating[4] = {0x1, 0x20, 0x300, 0x4000};
-  uint8_t results[32][16] = {{0}};
-  uint8_t results_count = 0;
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      if (i == j) /* all 4 rotating words must be present */
-        continue;
-      /* mix rotating word order */
-      uint64_t v[4] = {rotating[i], rotating[j], rotating[3 - i],
-                       rotating[3 - j]};
-      /* prepare vector against h64 = XXH_rotl64... */
-      v[0] = (v[0] >> 1) | (v[0] << (64 - 1));
-      v[1] = (v[1] >> 7) | (v[1] << (64 - 7));
-      v[2] = (v[2] >> 12) | (v[2] << (64 - 12));
-      v[3] = (v[3] >> 18) | (v[3] << (64 - 18));
-      /* attack v *= PRIME64_1 */
-      v[0] = v[0] * PRIME64_1_INV;
-      v[1] = v[1] * PRIME64_1_INV;
-      v[2] = v[2] * PRIME64_1_INV;
-      v[3] = v[3] * PRIME64_1_INV;
-      /* attack v = XXH_rotl64(v, 31) */
-      v[0] = (v[0] >> 31) | (v[0] << (64 - 31));
-      v[1] = (v[1] >> 31) | (v[1] << (64 - 31));
-      v[2] = (v[2] >> 31) | (v[2] << (64 - 31));
-      v[3] = (v[3] >> 31) | (v[3] << (64 - 31));
-      /* attack seed manipulation */
-      v[0] = v[0] - seed_manipulation[0];
-      v[1] = v[1] - seed_manipulation[1];
-      v[2] = v[2] - seed_manipulation[2];
-      v[3] = v[3] - seed_manipulation[3];
-      /* attack v += XXH_get64bits(p) * PRIME64_2 */
-      v[0] = v[0] * PRIME64_2_INV;
-      v[1] = v[1] * PRIME64_2_INV;
-      v[2] = v[2] * PRIME64_2_INV;
-      v[3] = v[3] * PRIME64_2_INV;
-      /* copy to results, if unique */
-      uint8_t unique = 1;
-      for (int t = 0; t < results_count; ++t) {
-        if (!memcmp(&results[0][t], v, 32))
-          unique = 0;
-      }
-      if (unique) {
-        memcpy(&results[0][results_count], v, 32);
-        ++results_count;
-      }
-    }
-  }
-  if (results_count) {
-    fprintf(stderr, "Created %u vectors, now testing...\n", results_count);
-    uint64_t origin = XXH64(&results[0][0], 32, 0);
-    for (int i = 0; i < results_count; ++i) {
-      words_push(&words, (fio_str_s)FIO_STR_INIT_STATIC2(&results[0][i], 32));
-      if (i && origin == XXH64(&results[0][i], 32, 0))
-        fprintf(stderr, "Possible collision [%d]\n", i);
-    }
-    fprintf(stderr, "Done testing.\n");
-  }
-}
-
-FIO_FUNC void add_bad4risky(void) {}
-
-FIO_FUNC void find_bit_collisions(hashing_func_fn fn, size_t collision_count,
-                                  uint8_t bit_count) {
+static void find_bit_collisions(hashing_func_fn fn, size_t collision_count,
+                                uint8_t bit_count) {
   words_s c = FIO_ARY_INIT;
   const uint64_t mask = (1ULL << bit_count) - 1;
   time_t start = clock();
@@ -691,15 +472,36 @@ FIO_FUNC void find_bit_collisions(hashing_func_fn fn, size_t collision_count,
   words_destroy(&c);
 }
 
-static void add_bad_words(void) {
-  if (!fio_cli_get("-t")) {
-    find_bit_collisions(risky, 16, 16);
-    find_bit_collisions(xxhash_test, 16, 16);
-    // find_bit_collisions(siphash13, 16, 16);
-    // find_bit_collisions(sha1, 16, 16);
+/* *****************************************************************************
+Finsing a mod64 inverse
+See: https://lemire.me/blog/2017/09/18/computing-the-inverse-of-odd-integers/
+***************************************************************************** */
+
+/* will return `inv` if `inv` is inverse of `n` */
+FIO_FUNC uint64_t inverse64_test(uint64_t n, uint64_t inv) {
+  uint64_t result = inv * (2 - (n * inv));
+  return result;
+}
+
+FIO_FUNC uint64_t inverse64(uint64_t x) {
+  uint64_t y = (3 * x) ^ 2;
+  y = inverse64_test(x, y);
+  y = inverse64_test(x, y);
+  y = inverse64_test(x, y);
+  y = inverse64_test(x, y);
+  if (FIO_LOG_LEVEL >= FIO_LOG_LEVEL_DEBUG) {
+    char buff[64];
+    fio_str_s t = FIO_STR_INIT;
+    fio_str_write(&t, "\n\t\tinverse for:\t", 16);
+    fio_str_write(&t, buff, fio_ltoa(buff, x, 16));
+    fio_str_write(&t, "\n\t\tis:\t\t\t", 8);
+    fio_str_write(&t, buff, fio_ltoa(buff, y, 16));
+    fio_str_write(&t, "\n\t\tsanity inverse test: 1==", 27);
+    fio_str_write_i(&t, x * y);
+    FIO_LOG_DEBUG("%s", fio_str_data(&t));
   }
-  add_bad4xxhash();
-  add_bad4risky();
+
+  return y;
 }
 
 /* *****************************************************************************
