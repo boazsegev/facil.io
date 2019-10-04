86d06eca8be57e5e1cfa1b999e08204c335209f2
Bo <bo@bowild.com>
2019-10-04 23:47:17 +0300

    typo

--- lib/facil/http/http.c
+++ lib/facil/http/http.c
@@ -632,9 +632,9 @@ static inline int http_test_encoded_path(const char *mem, size_t len) {
   while (mem < end && (pos = memchr(mem, '/', (size_t)len))) {
     len = end - pos;
     mem = pos + 1;
-    if (len >= 1 && pos[1] == '/')
+    if (pos[1] == '/')
       return -1;
-    if (len > 3 && pos[1] == '.' && pos[2] == '.' && pos[4] == '/')
+    if (len > 3 && pos[1] == '.' && pos[2] == '.' && pos[3] == '/')
       return -1;
   }
   return 0;
